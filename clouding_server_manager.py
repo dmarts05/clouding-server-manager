e91d5f3877d5d029b911611ce5c60baaf13b157a
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-05-02 14:40:45 +0200

    Refactor script with main function

--- clouding_server_manager.py
+++ clouding_server_manager.py
@@ -1,22 +1,29 @@
 import os
 import requests
 
-# Get the server id and api key from environment variables
-server_id = os.environ['CLOUDING_SERVER_ID']
-api_key = os.environ['CLOUDING_API_KEY']
+if __name__ == "__main__":
+    # Get the server id and api key from environment variables
+    server_id = os.environ.get("CLOUDING_SERVER_ID")
+    api_key = os.environ.get("CLOUDING_API_KEY")
 
-# Build the request
-url = f"https://api.clouding.io/v1/servers/{server_id}/archive"
-headers = {
-    "Content-Type": "application/json",
-    "X-API-KEY": api_key
-}
+    # Check if the server id and api key are valid
+    if server_id is None or api_key is None:
+        print(
+            "Please set the CLOUDING_SERVER_ID and CLOUDING_API_KEY environment variables")
+        exit(1)
 
-# Send the request
-response = requests.post(url, headers=headers)
+    # Build the request
+    url = f"https://api.clouding.io/v1/servers/{server_id}/archive"
+    headers = {
+        "Content-Type": "application/json",
+        "X-API-KEY": api_key
+    }
 
-# Check the response
-if response.ok:
-    print("Server archived successfully!")
-else:
-    print(f"Error archiving server: {response.text}")
+    # Send the request
+    response = requests.post(url, headers=headers)
+
+    # Check the response
+    if response.ok:
+        print("Server archived successfully!")
+    else:
+        print(f"Error archiving server: {response.text}")
