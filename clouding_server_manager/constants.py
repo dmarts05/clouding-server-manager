9cb08846e41185da6a7c9e7c28b27ebe8cd4bea9
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-05-26 00:04:14 +0200

    fix(commands): Fix unarchive and archive commands

--- clouding_server_manager/constants.py
+++ clouding_server_manager/constants.py
@@ -3,8 +3,14 @@
 BASE_URL = "https://api.clouding.io/v1"
 """The base URL of the API"""
 
-WAIT_TIME = 30
+WAIT_TIME = 15
 """The time to wait between each request to the API when archiving or unarchiving servers"""
 
 MAX_TOTAL_WAIT_TIME = 300
 """The maximum total time to wait for all servers to be unarchived or archived"""
+
+API_ALREADY_ARCHIVED_ERROR_MESSAGE = "Command 'archive' cannot be issued while server is in the current state"
+"""The error message returned by the API when trying to archive a server that is already archived"""
+
+API_ALREADY_UNARCHIVED_ERROR_MESSAGE = "Command 'unshelve' cannot be issued while server is in the current state"
+"""The error message returned by the API when trying to unarchive a server that is already unarchived"""
