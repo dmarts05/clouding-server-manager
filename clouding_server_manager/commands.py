e6261174597bc1dd9b9c7631fc7a6efb418eb386
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-05-26 00:04:14 +0200

    fix(commands): Fix unarchive and archive commands

--- clouding_server_manager/commands.py
+++ clouding_server_manager/commands.py
@@ -1,12 +1,9 @@
 import json
-import time
-from typing import Any, Dict, List
+from typing import List
 
 import click
-import requests
 
-from .constants import BASE_URL, MAX_TOTAL_WAIT_TIME, WAIT_TIME
-from .helpers import get_all_server_ids
+from .helpers import archive_servers_aux, list_servers_aux, unarchive_servers_aux
 
 
 @click.command()
@@ -46,32 +43,7 @@ def list_servers(api_key: str, targets: List[str], fields: List[str]) -> None:
     Raises:
         requests.RequestException: If there was an error with any of the requests
     """
-    click.secho("[LIST SERVERS] Listing servers...", fg="blue")
-
-    HEADERS = {"Content-Type": "application/json", "X-API-KEY": api_key}
-    REQUEST_URL = f"{BASE_URL}/servers"
-
-    responses_json: List[Dict[str, Any]] = []
-    # Check if the user wants to list all servers or just some of them by id
-    if "all" in targets:
-        response = requests.get(REQUEST_URL, headers=HEADERS)
-        if response.status_code == 200:
-            for server in response.json()["servers"]:
-                responses_json.append(server)
-        else:
-            raise requests.RequestException(f"Error: {response.status_code} {response.reason}")
-    else:
-        for target in targets:
-            response = requests.get(f"{REQUEST_URL}/{target}", headers=HEADERS)
-            if response.ok:
-                responses_json.append(response.json())
-            else:
-                raise requests.RequestException(f"Error: {response.status_code} {response.reason}")
-
-    # Filter the fields if the user specified any
-    if fields:
-        click.secho("Filtering fields...", fg="blue")
-        responses_json = [{field: server.get(field, "Invalid field") for field in fields} for server in responses_json]
+    responses_json = list_servers_aux(api_key, targets, fields)
 
     # Print the response result for each request (only if there were no errors)
     for r_json in responses_json:  # type: ignore
@@ -106,35 +78,7 @@ def archive_servers(api_key: str, targets: List[str]) -> None:
     Raises:
         requests.RequestException: If there was an error with any of the requests
     """
-    click.secho("[ARCHIVE SERVERS] Archiving servers...", fg="blue")
-
-    HEADERS = {"Content-Type": "application/json", "X-API-KEY": api_key}
-
-    responses_json: List[Dict[str, Any]] = []
-    if "all" in targets:
-        # Get all server ids if the user wants to unarchive all servers
-        targets = get_all_server_ids(api_key)
-
-    # Make unarchive requests until all servers have been unarchived or total wait time is exceeded
-    waited_time = 0
-    while targets and waited_time < MAX_TOTAL_WAIT_TIME:
-        for target in targets:
-            response = requests.post(f"{BASE_URL}/servers/{target}/archive", headers=HEADERS)
-
-            if response.ok and response.json()["status"] == "completed":
-                responses_json.append(response.json())
-                targets.remove(target)
-            elif not response.ok:
-                click.echo(json.dumps(response.json(), indent=4, sort_keys=True))
-                raise requests.RequestException(f"Error: {response.status_code} {response.reason}")
-
-        # Wait some time before making the next batch of requests
-        click.secho("[ARCHIVE SERVERS] Some servers are still being archived...", fg="blue")
-        click.secho(f"[ARCHIVE SERVERS] Waiting {WAIT_TIME} seconds before checking again...", fg="blue")
-        time.sleep(WAIT_TIME)
-        waited_time += WAIT_TIME
-
-    click.secho("[ARCHIVE SERVERS] All servers have been archived!", fg="blue")
+    responses_json = archive_servers_aux(api_key, targets)
 
     # Print the response result for each request (only if there were no errors)
     for r_json in responses_json:  # type: ignore
@@ -169,35 +113,7 @@ def unarchive_servers(api_key: str, targets: List[str]) -> None:
     Raises:
         requests.RequestException: If there was an error with any of the requests
     """
-    click.secho("[UNARCHIVE SERVERS] Unarchiving servers...", fg="blue")
-
-    HEADERS = {"Content-Type": "application/json", "X-API-KEY": api_key}
-
-    responses_json: List[Dict[str, Any]] = []
-    if "all" in targets:
-        # Get all server ids if the user wants to unarchive all servers
-        targets = get_all_server_ids(api_key)
-
-    # Make unarchive requests until all servers have been unarchived or total wait time is exceeded
-    waited_time = 0
-    while targets and waited_time < MAX_TOTAL_WAIT_TIME:
-        for target in targets:
-            response = requests.post(f"{BASE_URL}/servers/{target}/unarchive", headers=HEADERS)
-
-            if response.ok and response.json()["status"] == "completed":
-                responses_json.append(response.json())
-                targets.remove(target)
-            elif not response.ok:
-                click.echo(json.dumps(response.json(), indent=4, sort_keys=True))
-                raise requests.RequestException(f"Error: {response.status_code} {response.reason}")
-
-        # Wait some time before making the next batch of requests
-        click.secho("[UNARCHIVE SERVERS] Some servers are still being unarchived...", fg="blue")
-        click.secho(f"[UNARCHIVE SERVERS] Waiting {WAIT_TIME} seconds before checking again...", fg="blue")
-        time.sleep(WAIT_TIME)
-        waited_time += WAIT_TIME
-
-    click.secho("[UNARCHIVE SERVERS] All servers have been unarchived!", fg="blue")
+    responses_json = unarchive_servers_aux(api_key, targets)
 
     # Print the response result for each request (only if there were no errors)
     for r_json in responses_json:  # type: ignore
--- clouding_server_manager/helpers.py
+++ clouding_server_manager/helpers.py
@@ -1,29 +1,16 @@
-from typing import List
+import time
+from typing import Any, Dict, List
 
+import click
 import requests
 
-from .constants import BASE_URL
-
-# def send_request(api_key: str, action: Action, target: str) -> requests.Response:
-#     """Send the request to the API"""
-#     # Build the headers
-#     headers = {"Content-Type": "application/json", "X-API-KEY": api_key}
-
-#     # Build the url depending on the action
-#     if action == Action.ARCHIVE:
-#         url = f"https://api.clouding.io/v1/servers/{target}/archive"
-#         response = requests.post(url, headers=headers)
-#     elif action == Action.UNARCHIVE:
-#         url = f"https://api.clouding.io/v1/servers/{target}/unarchive"
-#         response = requests.post(url, headers=headers)
-#     elif action == Action.LIST:
-#         url = "https://api.clouding.io/v1/servers"
-#         response = requests.get(url, headers=headers)
-#     else:
-#         print("Error: Invalid action specified.")
-#         exit(1)
-
-#     return response
+from .constants import (
+    API_ALREADY_ARCHIVED_ERROR_MESSAGE,
+    API_ALREADY_UNARCHIVED_ERROR_MESSAGE,
+    BASE_URL,
+    MAX_TOTAL_WAIT_TIME,
+    WAIT_TIME,
+)
 
 
 def get_all_server_ids(api_key: str) -> List[str]:
@@ -49,3 +36,157 @@ def get_all_server_ids(api_key: str) -> List[str]:
     # Return a list with all the server ids
     server_ids = [server["id"] for server in response.json()["servers"]]
     return server_ids
+
+
+def list_servers_aux(api_key: str, targets: List[str], fields: List[str]) -> List[Dict[str, Any]]:
+    """
+    List all clouding servers or some of them by id (business logic)
+
+    Args:
+        api_key: The API key to use
+        targets: The target server or servers to perform the action on
+        fields: The fields to show in the responses
+
+    Raises:
+        requests.RequestException: If there was an error with any of the requests
+
+    Returns:
+        A list with all the responses to the listing requests
+    """
+    click.secho("[LIST SERVERS] Listing servers...", fg="blue")
+
+    HEADERS = {"Content-Type": "application/json", "X-API-KEY": api_key}
+    REQUEST_URL = f"{BASE_URL}/servers"
+
+    responses_json: List[Dict[str, Any]] = []
+    # Check if the user wants to list all servers or just some of them by id
+    if "all" in targets:
+        response = requests.get(REQUEST_URL, headers=HEADERS)
+        if response.status_code == 200:
+            for server in response.json()["servers"]:
+                responses_json.append(server)
+        else:
+            raise requests.RequestException(f"Error: {response.status_code} {response.reason}")
+    else:
+        for target in targets:
+            response = requests.get(f"{REQUEST_URL}/{target}", headers=HEADERS)
+            if response.ok:
+                responses_json.append(response.json())
+            else:
+                raise requests.RequestException(f"Error: {response.status_code} {response.reason}")
+
+    # Filter the fields if the user specified any
+    if fields:
+        click.secho("[LIST SEVERS] Filtering fields...", fg="blue")
+        responses_json = [{field: server.get(field, "Invalid field") for field in fields} for server in responses_json]
+
+    return responses_json
+
+
+def archive_servers_aux(api_key: str, targets: List[str]) -> List[Dict[str, Any]]:
+    """
+    Archive all clouding servers or some of them by id (business logic)
+
+    Args:
+        api_key: The API key to use
+        targets: The target server or servers to perform the action on
+
+    Raises:
+        requests.RequestException: If there was an error with any of the requests
+
+    Returns:
+        A list with all the responses to the archiving requests
+    """
+    click.secho("[ARCHIVE SERVERS] Archiving servers...", fg="blue")
+
+    HEADERS = {"Content-Type": "application/json", "X-API-KEY": api_key}
+
+    responses_json: List[Dict[str, Any]] = []
+    if "all" in targets:
+        # Get all server ids if the user wants to archive all servers
+        targets = get_all_server_ids(api_key)
+
+    # Make unarchive requests until all servers have been archived or total wait time is exceeded
+    waited_time = 0
+    while targets and waited_time < MAX_TOTAL_WAIT_TIME:
+        for target in targets:
+            response = requests.post(f"{BASE_URL}/servers/{target}/archive", headers=HEADERS)
+
+            if response.ok:
+                responses_json.append(response.json())
+            # If there is an error, we check if it is because the server is already archived
+            elif (
+                response.status_code == 400
+                and response.json()["errors"]["server-state"][0] == API_ALREADY_ARCHIVED_ERROR_MESSAGE
+            ):
+                # If the server is already archived or has been archived, we remove it from the targets list
+                targets.remove(target)
+            elif not response.ok:
+                raise requests.RequestException(f"Error: {response.status_code} {response.reason}")
+
+        # Wait some time before making the next batch of requests if there are still servers to archive
+        if targets:
+            click.secho("[ARCHIVE SERVERS] Some servers are still being archived...", fg="blue")
+            click.secho(f"[ARCHIVE SERVERS] Waiting {WAIT_TIME} seconds before checking again...", fg="blue")
+            time.sleep(WAIT_TIME)
+            waited_time += WAIT_TIME
+
+    click.secho("[ARCHIVE SERVERS] All servers have been archived!", fg="blue")
+
+    # Change server status to completed in the responses
+    responses_json = [{**response, "status": "completed"} for response in responses_json]
+
+    return responses_json
+
+
+def unarchive_servers_aux(api_key: str, targets: List[str]) -> List[Dict[str, Any]]:
+    """
+    Unarchive all clouding servers or some of them by id (business logic)
+
+    Args:
+        api_key: The API key to use
+        targets: The target server or servers to perform the action on
+
+    Raises:
+        requests.RequestException: If there was an error with any of the requests
+    """
+    click.secho("[UNARCHIVE SERVERS] Unarchiving servers...", fg="blue")
+
+    HEADERS = {"Content-Type": "application/json", "X-API-KEY": api_key}
+
+    responses_json: List[Dict[str, Any]] = []
+    if "all" in targets:
+        # Get all server ids if the user wants to unarchive all servers
+        targets = get_all_server_ids(api_key)
+
+    # Make unarchive requests until all servers have been unarchived or total wait time is exceeded
+    waited_time = 0
+    while targets and waited_time < MAX_TOTAL_WAIT_TIME:
+        for target in targets:
+            response = requests.post(f"{BASE_URL}/servers/{target}/unarchive", headers=HEADERS)
+
+            if response.ok:
+                responses_json.append(response.json())
+            # If there is an error, we check if it is because the server is already unarchived
+            elif (
+                response.status_code == 400
+                and response.json()["errors"]["server-state"][0] == API_ALREADY_UNARCHIVED_ERROR_MESSAGE
+            ):
+                # If the server is already unarchived or has been unarchived, we remove it from the targets list
+                targets.remove(target)
+            elif not response.ok:
+                raise requests.RequestException(f"Error: {response.status_code} {response.reason}")
+
+        # Wait some time before making the next batch of requests if there are still servers to unarchive
+        if targets:
+            click.secho("[UNARCHIVE SERVERS] Some servers are still being unarchived...", fg="blue")
+            click.secho(f"[UNARCHIVE SERVERS] Waiting {WAIT_TIME} seconds before checking again...", fg="blue")
+            time.sleep(WAIT_TIME)
+            waited_time += WAIT_TIME
+
+    click.secho("[UNARCHIVE SERVERS] All servers have been unarchived!", fg="blue")
+
+    # Change server status to completed in the responses
+    responses_json = [{**response, "status": "completed"} for response in responses_json]
+
+    return responses_json
