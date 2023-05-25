9eefd096aa1de8a623de9cd153ae46a1b70f9774
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-05-25 22:23:40 +0200

    feat(commands): Add remaining commands (WIP)

--- clouding_server_manager/constants.py
+++ clouding_server_manager/constants.py
@@ -1,2 +1,10 @@
 """This module contains the constants used in the project."""
+
 BASE_URL = "https://api.clouding.io/v1"
+"""The base URL of the API"""
+
+WAIT_TIME = 30
+"""The time to wait between each request to the API when archiving or unarchiving servers"""
+
+MAX_TOTAL_WAIT_TIME = 300
+"""The maximum total time to wait for all servers to be unarchived or archived"""
